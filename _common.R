d6eea8f5e8e11b746180293d224c8a2a22009164
Martin Frigaard <mjfrigaard@pm.me>
2026-05-15 08:55:04 -0700

    first commit

---
+++ _common.R
@@ -0,0 +1,104 @@
+set.seed(1014)
+
+# knitr settings ----
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  fig.retina = 2,
+  fig.width = 6,
+  fig.asp = 2/3,
+  fig.show = "hold"
+)
+
+# options ----
+options(
+  dplyr.print_min = 6,
+  dplyr.print_max = 6,
+  pillar.bold = TRUE,
+  width = 60,
+  scipen = 9999
+)
+
+# Not in ----
+"%nin%" <- function(x, y) {
+  return(!(x %in% y))
+}
+
+co_box <- function(
+  color = "b",
+  header = "header",
+  contents = "Your text",
+  size = "1.10",
+  hsize = "1.15",
+  fold = FALSE,
+  look = "default") {
+  
+  if (look == "simple") {
+    look <- "simple"
+  } else if (look == "minimal") {
+    look <- "minimal"
+  } else {
+    look <- "default"
+  }
+  
+  fold <- tolower(as.character(fold))
+  size <- as.character(size)
+  
+  class <- switch(color,
+    b = "note",
+    g = "tip",
+    r = "important",
+    o = "warning",
+    y = "caution",
+    stop("Invalid `type`", call. = FALSE)
+  )
+  
+  switch(color,
+    b = cat(paste0(
+      "\n\n",
+      ":::: {.callout-", class, " collapse='", fold, "'", " appearance='", look, "' icon=false}", "\n\n",
+      "## [", header, "]{style='font-weight: bold; font-size: ", hsize, "em;'}\n\n",
+      "::: {style='font-size: ", size, "em; color: #282b2d;'}\n\n",
+      "\n", glue::glue_collapse(contents), "\n\n",
+      "::: \n\n",
+      "::::", "\n"
+    )),
+    g = cat(paste0(
+      "\n\n",
+      ":::: {.callout-", class, " collapse='", fold, "'", " appearance='", look, "' icon=false}", "\n\n",
+      "## [", header, "]{style='font-weight: bold; font-size: ", hsize, "em;'}\n\n",
+      "::: {style='font-size: ", size, "em; color: #282b2d;'}\n\n",
+      "\n", glue::glue_collapse(contents), "\n\n",
+      "::: \n\n",
+      "::::", "\n"
+    )),
+    y = cat(paste0(
+      "\n\n",
+      ":::: {.callout-", class, " collapse='", fold, "'", " appearance='", look, "' icon=false}", "\n\n",
+      "## [", header, "]{style='font-weight: bold; font-size: ", hsize, "em;'}\n\n",
+      "::: {style='font-size: ", size, "em; color: #282b2d;'}\n\n",
+      "\n", glue::glue_collapse(contents), "\n\n",
+      "::: \n\n",
+      "::::", "\n"
+    )),
+    o = cat(paste0(
+      "\n\n",
+      ":::: {.callout-", class, " collapse='", fold, "'", " appearance='", look, "' icon=false}", "\n\n",
+      "## [", header, "]{style='font-weight: bold; font-size: ", hsize, "em;'}\n\n",
+      "::: {style='font-size: ", size, "em; color: #282b2d;'}\n\n",
+      "\n", glue::glue_collapse(contents), "\n\n",
+      "::: \n\n",
+      "::::", "\n"
+    )),
+    r = cat(paste0(
+      "\n\n",
+      ":::: {.callout-", class, " collapse='", fold, "'", " appearance='", look, "' icon=false}", "\n\n",
+      "## [", header, "]{style='font-weight: bold; font-size: ", hsize, "em;'}\n\n",
+      "::: {style='font-size: ", size, "em; color: #282b2d;'}\n\n",
+      "\n", glue::glue_collapse(contents), "\n\n",
+      "::: \n\n",
+      "::::", "\n"
+    )),
+    stop("Invalid `type`", call. = FALSE)
+  )
+}
